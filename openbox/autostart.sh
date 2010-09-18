55a1bed82c70736071e82dfab4e57434bef33314
Bruno Michel <bmichel@menfin.info>
2010-09-18 16:50:07 +0200

    Ubuntu Maverick

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -9,23 +9,19 @@ numlockx on
 # alsactl restore
 # xmodmap ~/config/xmodmaprc
 # xmodmap -e "pointer = 1 2 3 6 7 4 5"
-Esetroot -f ~/Documents/Wallpapers/vladstudio_frog_1600x1200.jpg &
-# ~/.scripts/dlbg.sh commons
-# ~/.scripts/random_wallpaper.rb
+Esetroot -f ~/Documents/Wallpapers/vladstudio_frog_1600x1200.jpg
 ~/.scripts/setlayout 0 2 2 0
 
 # Programs to launch at startup
 gtk-redshift -l 48.8:2.3 -t 6500:5800 &
-firefox &
+firefox-4.0 &
 thunderbird &
 liferea &
 #gajim &
-gwibber-service &
-#exaile &
+#gwibber-service &
 urxvt -name irssi -title irssi -e irssi &
 urxvt -name gruik -title gruik &
 urxvt -name codaz -title codaz &
 
 # Programs that will run after Openbox has started
-(sleep 2 && fbpanel) &
-
+(sleep 1 && fbpanel) &
---
+++ vim/ftplugin/gitconfig.vim
@@ -0,0 +1,1 @@
+iab [master] [branch "master"]remote = originmerge = refs/heads/master
