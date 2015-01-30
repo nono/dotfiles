64072744cb9e4ca15f98dda5b8d696f84185fddc
Bruno Michel <bmichel@menfin.info>
2015-01-30 16:04:55 +0100

    Add vim spell files

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -13,9 +13,12 @@ setlayout 0 2 2 0
 sleep 2
 x-www-browser &
 thunderbird &
-urxvt -name irssi -title irssi -e irssi &
+urxvt -name irssi -title irssi &
 urxvt -name gruik -title gruik &
 urxvt -name codaz -title codaz &
+urxvt -name codaz -title codaz &
+urxvt -name codaz -title codaz &
+urxvt -name nvim  -title nvim  &
 
 # Programs that will run after Openbox has started
 (sleep 1 && fbpanel) &
