3415d1dcc5acf704001454b0c3f7c15cdbd5c057
Bruno Michel <bmichel@menfin.info>
2018-03-10 23:23:29 +0100

    Try tilix

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -13,12 +13,8 @@ setlayout 0 2 2 0
 sleep 2
 x-www-browser &
 thunderbird &
-urxvt -name irssi  -title irssi  &
-urxvt -name gruik  -title gruik  &
-urxvt -name codaz1 -title codaz1 &
-urxvt -name codaz2 -title codaz2 &
-urxvt -name codaz3 -title codaz3 &
-urxvt -name nvim   -title nvim   &
+tilix --maximize -t gruik -s gruik -w Desktop &
+tilix --maximize -t codaz -s codaz -w ~stack &
 docker start cozy-stack-couch &
 
 # Programs that will run after Openbox has started
